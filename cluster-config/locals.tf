93ad468091d41195763fffaf8422c05afd5e1e13
Colin Haywood <colin.haywood@datamasque.com>
2025-10-03 16:24:29 +1300

    Update to pull images from new public ECR

--- cluster-config/locals.tf
+++ cluster-config/locals.tf
@@ -5,16 +5,21 @@ locals {
 
   ecr_image_url = {
     for cluster_key, cluster_config in lookup(local.ecs_config["ecs"], "clusters", {}) : cluster_key => {
-      account_id = cluster_config["ecr"]["ecrRepo"] == "public" ? "269378400967" : data.aws_caller_identity.current.account_id
-      region     = cluster_config["ecr"]["ecrRepo"] == "public" ? cluster_config["ecr"]["ecrRepoRegion"] : data.aws_region.current.name
-      repo_base  = cluster_config["ecr"]["ecrRepo"] == "public" ? "datamasque" : cluster_config["ecr"]["ecrRepoName"]
+      is_public  = cluster_config["ecr"]["ecrRepo"] == "public"
+      account_id = cluster_config["ecr"]["ecrRepo"] == "public" ? null : data.aws_caller_identity.current.account_id
+      region     = cluster_config["ecr"]["ecrRepo"] == "public" ? null : data.aws_region.current.name
+      repo_base  = cluster_config["ecr"]["ecrRepo"] == "public" ? "u4t6n7u6/datamasque" : cluster_config["ecr"]["ecrRepoName"]
       image_tag  = cluster_config["ecr"]["ecrImageTag"]
     }
   }
 
-  # ECR base URL for all images (`<account ID>.dkr.ecr.<region>.amazonaws.com/<repo prefix>`)
+  # ECR base URL for all images
+  # Public ECR: `public.ecr.aws/<repo prefix>`
+  # Private ECR: `<account ID>.dkr.ecr.<region>.amazonaws.com/<repo prefix>`
   ecr_base_url = {
     for cluster_key in keys(local.ecr_image_url) : cluster_key =>
-      "${local.ecr_image_url[cluster_key].account_id}.dkr.ecr.${local.ecr_image_url[cluster_key].region}.amazonaws.com/${local.ecr_image_url[cluster_key].repo_base}"
+      local.ecr_image_url[cluster_key].is_public ?
+        "public.ecr.aws/${local.ecr_image_url[cluster_key].repo_base}" :
+        "${local.ecr_image_url[cluster_key].account_id}.dkr.ecr.${local.ecr_image_url[cluster_key].region}.amazonaws.com/${local.ecr_image_url[cluster_key].repo_base}"
   }
 }
